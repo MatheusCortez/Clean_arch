5d93e59d6d5ac4011c72334008d0650b94a17dfb
Matheus Cortez <matheus.cortez@live.com>
2023-12-18 22:14:46 -0300

    test:created env config service tests

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,21 +1,17 @@
 import { DynamicModule, Module } from '@nestjs/common'
 import { EnvConfigService } from './env-config.service'
-import {
-    ConfigModule,
-    ConfigModuleOptions,
-    ConfigService,
-} from '@nestjs/config'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { join } from 'path'
 
 @Module({
-    providers: [EnvConfigService, ConfigService],
+    providers: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
     static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
         return super.forRoot({
             ...options,
             envFilePath: [
-                join(__dirname, `../../../.env.${process.env.NODE_ENV}`),
+                join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
             ],
         })
     }
--- src/shared/infrastructure/env-config/env-config.service.spec.ts
+++ src/shared/infrastructure/env-config/env-config.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { EnvConfigModule } from './env-config.module'
 import { EnvConfigService } from './env-config.service'
 
-describe('EnvConfigService', () => {
-    let service: EnvConfigService
+describe('EnvConfigService unit tests', () => {
+    let sut: EnvConfigService
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [EnvConfigModule.forRoot()],
             providers: [EnvConfigService],
         }).compile()
 
-        service = module.get<EnvConfigService>(EnvConfigService)
+        sut = module.get<EnvConfigService>(EnvConfigService)
     })
 
     it('should be defined', () => {
-        expect(service).toBeDefined()
+        expect(sut).toBeDefined()
+    })
+
+    it('should return the variable PORT', () => {
+        expect(sut.getAppPort()).toBe(3030)
+    })
+
+    it('should return the variable NODE_ENV', () => {
+        expect(sut.getNodeEnv()).toBe('test')
     })
 })
