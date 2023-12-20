ff43d481fef5c26b0b519d9a4c95802156930157
Matheus Cortez <matheus.cortez@live.com>
2023-12-19 21:32:34 -0300

    test: created test of getters methods

--- src/users/entities/tests/unit/user.entity.spec.ts
+++ src/users/entities/tests/unit/user.entity.spec.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker'
 describe('User Entity unit Tests', () => {
     let props: UserProps
     let suv: UserEntity
-    it('constructor method', async () => {
+    beforeEach(() => {
         props = {
             name: faker.person.fullName(),
             email: faker.internet.email(),
@@ -12,9 +12,33 @@ describe('User Entity unit Tests', () => {
         }
 
         suv = new UserEntity(props)
+    })
+    it('Should be returned props inputed in UserEntity', async () => {
         expect(suv.props.name).toBe(props.name)
         expect(suv.props.email).toBe(props.email)
         expect(suv.props.password).toBe(props.password)
         expect(suv.props.createdAt).toBeInstanceOf(Date)
+    })
+
+    describe('Getters', () => {
+        it('Should be returned name in UserEntity', () => {
+            expect(suv.props.name).toBeDefined()
+            expect(suv.props.name).toBe(props.name)
+            expect(typeof suv.props.name).toBe('string')
+        })
+
+        it('Should be returned email in UserEntity', () => {
+            expect(suv.props.email).toBeDefined()
+            expect(suv.props.email).toBe(props.email)
+            expect(typeof suv.props.email).toBe('string')
+        })
+        it('Should be returned password in UserEntity', () => {
+            expect(suv.props.password).toBeDefined()
+            expect(typeof suv.props.password).toBe('string')
+        })
+        it('Should be returned createdAt in UserEntity', () => {
+            expect(suv.props.createdAt).toBeDefined()
+            expect(suv.props.createdAt).toBeInstanceOf(Date)
+        })
     })
 })
